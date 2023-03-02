08b0d0dd2db33f0f2fc10a178f913aa987e7e5fb
Thanhhx1510 <thanhhaha1510@gmail.com>
2023-03-02 13:21:46 +0700

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,5 +10,7 @@ namespace API.Entities
         public int Id { get; set; }
         public string UserName { get; set; }
         
+        public byte[] PassWordHash { get; set; }
+        public byte[] PassWordSalt { get; set; }
     }
 }
